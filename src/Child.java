ae2800feb5ca0be9035b38aa45f23fe8fb389d95
Dmytro Kovalevskyi <dmytro.kovalevskyi@airslate.com>
2023-05-30 14:15:09 +0300

    Step 2 correcition

--- src/Child.java
+++ src/Child.java
@@ -1,23 +1,14 @@
-import java.io.IOException;
-
 public class Child extends Parent {
 
-    public Child(String constructorMessage) throws IOException {
+    public Child(String constructorMessage) {
         super(constructorMessage);
-        fw.write("\n" + constructorMessage + " message from Child constructor");
     }
 
     {
         System.out.println("This is message from Child initialization block");
-        fw.write("\n message from Child init block");
     }
 
     static {
         System.out.println("This is message from Child static initializaion block");
-        try {
-            fw.write("\n message from Child static init block");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
